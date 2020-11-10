584c6648433a54476a0167b874fc053838962447
Jeffesson Gomes <jeffessong4@gmail.com>
2020-11-10 15:53:03 -0300

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Home from './Pages/Home';
+
+import Navbar from './components/Navbar';
+
+const App = () => {
+  return (
+    <>
+      <Home />
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Container = styled.nav`
+  div.container{
+    border-bottom: 1px solid #FFF;
+
+    div {
+      ul{
+        li{
+          font: var(--unnamed-font-style-normal) normal var(--unnamed-font-weight-bold) var(--unnamed-font-size-14)/var(--unnamed-line-spacing-24) var(--unnamed-font-family-muli);
+          letter-spacing: var(--unnamed-character-spacing-0);
+          font: normal normal bold 14px/24px Muli;
+          letter-spacing: 0px;
+          
+          a{
+            color: #FFF !important;
+          }
+        }
+      }
+    }
+  }
+`;
